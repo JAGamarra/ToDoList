b8363a8ea8e3d216f5b62c687dc6f289c07fe187
Julian Gamarra <gmrrajulian@gmail.com>
2021-10-30 23:12:35 -0500

    Se agrega la funcion actualizar

--- js/myList.js
+++ js/myList.js
@@ -4,33 +4,61 @@ const updButton = document.querySelector("#updateBtn");
 const listSite = document.querySelector("#myList");
 const form = document.querySelector("#form");
 let myList = [];
+const pendingObj = {
+  id: "",
+  pending: "",
+};
+let editionMode = false;
 
 startEvents();
 function startEvents() {
   sendButton.addEventListener("click", addToList);
+  updButton.addEventListener("click", updateBtnEvent);
+  myPending.addEventListener("input", fillPendingObj);
   document.addEventListener("DOMContentLoaded", () => {
     myList = JSON.parse(localStorage.getItem("pendings")) || [];
 
+    buttonController();
     createListHTML();
   });
 }
 
-function addToList() {
-  console.log("Agregando a lista");
-  if (myPending.value === "") {
-    console.log("No puede ir vacío");
+function fillPendingObj(e) {
+  pendingObj[e.target.name] = e.target.value;
+  console.log(pendingObj);
+  const keyCode = e.keyCode || e.which;
+  if (keyCode == "13") {
+    e.preventDefault();
+    console.log("Presionas Enter");
+  }
+}
+
+function addToList(e) {
+  e.preventDefault();
+  const { pending } = pendingObj;
+  if (pending === "") {
+    showMessage("You must write a task before add it", "error");
     return;
   }
-  const pendingObj = {
-    id: Date.now(),
-    pending: myPending.value,
-  };
 
-  myList = [...myList, pendingObj];
+  pendingObj.id = Date.now();
 
-  console.log(pendingObj);
+  myList = [...myList, { ...pendingObj }];
+
   createListHTML();
+  showMessage("Added successfully", "success");
+  restartObject();
+  form.reset();
+}
 
+function updateBtnEvent(e) {
+  e.preventDefault();
+  updateTask({ ...pendingObj });
+  createListHTML();
+  showMessage("Updated successfully", "success");
+  editionMode = false;
+  restartObject();
+  buttonController();
   form.reset();
 }
 
@@ -49,25 +77,31 @@ function createListHTML() {
       const iconContainer = document.createElement("div");
       iconContainer.classList.add("iconsContainer");
 
-      const aCheck = document.createElement("a");
+      const aCheck = document.createElement("button");
       aCheck.setAttribute("id", "check");
-      aCheck.setAttribute("href", "#");
       const spanCheck = document.createElement("span");
       spanCheck.classList.add("material-icons", "md-24", "green");
       spanCheck.innerText = "task_alt";
       aCheck.appendChild(spanCheck);
 
-      const aUpdate = document.createElement("a");
+      aCheck.onclick = (e) => {
+        e.preventDefault();
+      };
+
+      const aUpdate = document.createElement("button");
       aUpdate.setAttribute("id", "update");
-      aUpdate.setAttribute("href", "#");
       const spanUpdate = document.createElement("span");
       spanUpdate.classList.add("material-icons", "md-24", "yellow");
       spanUpdate.innerText = "autorenew";
       aUpdate.appendChild(spanUpdate);
 
-      const aDelete = document.createElement("a");
+      aUpdate.onclick = (e) => {
+        e.preventDefault();
+        editTask(listPending);
+      };
+
+      const aDelete = document.createElement("button");
       aDelete.setAttribute("id", "delete");
-      aDelete.setAttribute("href", "#");
       const spanDelete = document.createElement("span");
       spanDelete.classList.add("material-icons", "md-24", "red");
       spanDelete.innerHTML = "delete_outline";
@@ -83,10 +117,8 @@ function createListHTML() {
       iconContainer.appendChild(aDelete);
       li.appendChild(textSpan);
       li.appendChild(iconContainer);
-      console.log(li);
 
       listSite.appendChild(li);
-      console.log(listPending.pending);
     });
   }
   setPendingsOnLS();
@@ -100,9 +132,75 @@ function deletePrevHTML() {
 
 function deletePending(id) {
   myList = myList.filter((listItem) => listItem.id !== id);
+  editionMode = false;
   createListHTML();
+  restartObject();
+  buttonController();
+  form.reset();
 }
 
 function setPendingsOnLS() {
   localStorage.setItem("pendings", JSON.stringify(myList));
+}
+
+function editTask(taskObj) {
+  const { id, pending } = taskObj;
+  myPending.value = pending;
+
+  pendingObj.id = id;
+  pendingObj.pending = pending;
+
+  editionMode = true;
+  buttonController();
+}
+
+function showMessage(message, type) {
+  const divMessage = document.createElement("div");
+  divMessage.classList.add("messageInfo");
+  if (type === "error") {
+    divMessage.textContent = message;
+    divMessage.classList.remove("success");
+    divMessage.classList.add("error");
+  } else {
+    divMessage.textContent = message;
+    divMessage.classList.remove("error");
+    divMessage.classList.add("success");
+  }
+
+  document.querySelector(".form-container").insertBefore(divMessage, form);
+
+  setTimeout(() => {
+    divMessage.remove();
+  }, 2700);
+}
+
+function buttonController() {
+  if (editionMode) {
+    sendButton.setAttribute("disabled", "");
+    updButton.removeAttribute("disabled");
+
+    sendButton.classList.add("btnDisabled");
+    sendButton.classList.remove("btnForm");
+
+    updButton.classList.add("btnForm");
+    updButton.classList.remove("btnDisabled");
+  } else {
+    sendButton.removeAttribute("disabled");
+    updButton.setAttribute("disabled", "");
+
+    sendButton.classList.add("btnForm");
+    sendButton.classList.remove("btnDisabled");
+
+    updButton.classList.add("btnDisabled");
+    updButton.classList.remove("btnForm");
+  }
+}
+
+function restartObject() {
+  pendingObj.id = "";
+  pendingObj.pending = "";
+}
+
+function updateTask(pending) {
+  myList = myList.map((task) => (task.id === pending.id ? pending : task));
 }
